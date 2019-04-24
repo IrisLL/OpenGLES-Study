5aafda685af38e6c737468ba14055298d384c555
IrisLL <sara528@sina.cn>
2019-04-24 16:51:09 +0800

    彩色等腰三角形
    1、添加顶点坐标矩阵变换
    2、添加 由顶点向片元 传递 颜色改变

--- OpenGLStudy/app/src/main/java/com/example/openglstudy/FGLRender.java
+++ OpenGLStudy/app/src/main/java/com/example/openglstudy/FGLRender.java
@@ -2,6 +2,7 @@ package com.example.openglstudy;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.util.Log;
 import android.view.View;
 
@@ -13,20 +14,30 @@ import javax.microedition.khronos.opengles.GL10;
 public class FGLRender implements GLSurfaceView.Renderer {
 
     private Triangel triangle;
+    private IsoscelesTriangle isoscelesTriangle;
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
         GLES20.glClearColor(0.5f,0.5f,0.5f,1.0f);
         Log.i("iris","onSurfaceCreated");
-        triangle=new Triangel();
+      //  triangle=new Triangel();
+        isoscelesTriangle=new IsoscelesTriangle();
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
 
         Log.i("iris","onSurfaceChanged");
-        GLES20.glViewport(0,0,width,height);
+       // GLES20.glViewport(0,0,width,height);
+        //计算宽高比
+        float ratio=(float)width/height;
+        //设置透视投影
+        Matrix.frustumM(IsoscelesTriangle.mProjectMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        //设置相机位置
+        Matrix.setLookAtM(IsoscelesTriangle.mViewMatrix, 0, 0, 0, 7.0f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        //计算变换矩阵
+        Matrix.multiplyMM(IsoscelesTriangle.mMVPMatrix,0,IsoscelesTriangle.mProjectMatrix,0,IsoscelesTriangle.mViewMatrix,0);
 
     }
 
@@ -35,7 +46,8 @@ public class FGLRender implements GLSurfaceView.Renderer {
 
         Log.e("iris","onDrawFrame");
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT|GLES20.GL_DEPTH_BUFFER_BIT);
-        triangle.Draw();
+       // triangle.Draw();
+        isoscelesTriangle.Draw();
     }
 
 
